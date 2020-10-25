f7b42837f61d158ff7c73221e95f8dbd182dc096
Poh Leanne Kee <pohleanne.16@gmail.com>
2020-10-24 19:37:00 -0700

    Add files via upload
    changes made in defPopup and popup.js

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+  console.log("Chrome extension");
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+
